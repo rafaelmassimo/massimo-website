0f0928ee0b2cfc2351c245d8207b74b0832f3340
Rafael Massimo <rafael_massimo@hotmail.com>
2024-10-30 21:49:33 -0700

    add zoom feature to the image

--- my-app/app/components/Slider.tsx
+++ my-app/app/components/Slider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Navigation, Pagination } from 'swiper/modules';
@@ -9,11 +9,25 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
 import { ColorRing } from 'react-loader-spinner';
+
 const Slider = ({ images }: { images: string[] }) => {
 	const [loadedImageCount, setLoadedImageCount] = useState<number>(0);
+	const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+	const [fullScreenImage, setFullScreenImage] = useState<string | null>(null);
 
 	const handleImageLoad = () => {
 		setLoadedImageCount((prev) => prev + 1);
+	};
+
+	const toggleFullScreen = (image: string) => {
+		// Toggle full-screen mode by setting or clearing the full-screen image
+		if (isFullScreen && fullScreenImage === image) {
+			setIsFullScreen(false);
+			setFullScreenImage(null);
+		} else {
+			setFullScreenImage(image);
+			setIsFullScreen(true);
+		}
 	};
 
 	return (
@@ -34,27 +48,41 @@ const Slider = ({ images }: { images: string[] }) => {
 					<SwiperSlide key={index}>
 						<div className="flex h-full w-full items-center justify-center relative">
 							{/* Check if the current image is loaded */}
-							{loadedImageCount <images.length ? (
+							{loadedImageCount < images.length ? (
 								<div className="absolute z-10 flex justify-center items-center bg-white/80 h-full w-full">
 									<ColorRing colors={['#8e8484', '#d7d7d7', '#742008', '#9a1607', '#c50202']} />
-									
 								</div>
 							) : null}
 
 							<Image
 								src={image}
 								alt="image"
-								className="block h-full object-cover"
+								className="block h-full object-cover cursor-zoom-in"
 								sizes="100%"
 								fill
 								placeholder="blur"
 								blurDataURL={`data:image/jpeg;base64,${image}`}
 								onLoad={handleImageLoad}
+								onClick={() => toggleFullScreen(image)}
 							/>
 						</div>
 					</SwiperSlide>
 				))}
 			</Swiper>
+
+			{/* Full-screen overlay */}
+			{isFullScreen && fullScreenImage && (
+				<div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-95 rounded-xl ">
+					<Image
+						src={fullScreenImage}
+						alt="Full Screen"
+						className="cursor-zoom-out rounded-xl object-contain" // Ensures aspect ratio is preserved
+						onClick={() => toggleFullScreen(fullScreenImage)}
+						fill
+						
+					/>
+				</div>
+			)}
 		</div>
 	);
 };
